f769383f10151645525830d3c965ec661d7b30be
Classic-username <josh.drapergame@gmail.com>
2024-02-06 16:28:21 -0700

    Added Layout, /customers, and /invoices routes

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return <p>Invoices Page</p>
+};
+
+//I believe this exists within the "ui" folder, on the same level as "dashboard" but for some reason the tutorial is asking us to create the invoice and customer page folders again?
